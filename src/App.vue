d6c6c97658d485b1845acbdc11d1cb369705151d
Romek Turaj <romanturaj@gmail.com>
2018-11-10 19:58:26 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="container">
+      <appHeader :quotesCount="quotes.length" :maxQuotes="maxQuotes"></appHeader>
+      <appNewQuote @quoteAdded="addQuote"></appNewQuote>
+      <appQuoteGrid @quoteDeleted="deleteQuote":quotes="quotes"></appQuoteGrid>
+      <appInfoBox></appInfoBox>
+    </div>
+</template>
+
+<script>
+    import NewQuote from './components/NewQuote.vue';
+    import QuoteGrid from './components/QuoteGrid.vue';
+    import InfoBox from './components/InfoBox.vue';
+    import Header from './components/Header.vue';
+    export default {
+      data: function() {
+        return {
+          maxQuotes: 10,
+          quotes: ['test quote','test quote2']
+        }
+      },
+      components: {
+        appQuoteGrid: QuoteGrid,
+        appNewQuote: NewQuote,
+        appInfoBox: InfoBox,
+        appHeader: Header
+      },
+      methods: {
+        addQuote(quote){
+          if (this.quotes.length >= this.maxQuotes) {
+            alert('Delete some quote first')
+          } else {
+          this.quotes.push(quote);
+        }
+        },
+        deleteQuote(index){
+          this.quotes.splice(index,1);
+        }
+      }
+
+    }
+</script>
+
+<style>
+</style>
---
+++ src/components/NewQuote.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="row">
+    <div class="col-sm-12 form group">
+      <label>New Quote</label>
+      <textarea class="form-control" rows="3" v-model="quote"></textarea>
+    </div>
+    <div class="col-sm-12 text-center">
+      <button class="btn btn-primary" @click="createNew">Add Quote</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      quote: ''
+    }
+  },
+  methods:{
+    createNew(){
+      this.$emit('quoteAdded',this.quote);
+      this.quote = '';
+    }
+  }
+
+}
+
+</script>
+
+<style scoped>
+  .col-sm-12 {
+    margin: 10px;
+  }
+</style>
